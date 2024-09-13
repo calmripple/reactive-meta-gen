a15495efbffad1424572f02ca10cde6cdd73f21c
calmripple <ripple@dmsrs.xyz>
2024-09-13 09:11:13 +0800

    依赖更新：提升开发依赖项至最新版本
    - 升级`@types/node`至22.5.4
    - 更新`@types/vscode`至1.93.0
    - 将`bumpp`版本提升至9.5.2
    - 将`eslint`更新至9.10.0
    - 升级`lint-staged`至15.2.10
    - 将`taze`版本升级至0.16.8
    - 更新`typescript`至5.6.2
    - 将`unbuild`更新至2.0.0
    - 将`vitest`更新至2.1.0
    
    以上更新确保了项目中使用的开发依赖项的最新状态，包含了重要的安全和功能改进。

--- test/useSample.ts
+++ test/useSample.ts
@@ -1,4 +1,4 @@
-import { useConfig, useCommandBaseBase, useCommandAddWatchDir } from './output/xyz-configuration-array'
+import { useConfig, useCommandBase, useCommandAddWatchDir } from './output/xyz-configuration-array'
 import { defineExtension } from 'reactive-vscode'
 
 const { activate, deactivate } = defineExtension(() => {
@@ -7,7 +7,7 @@ const { activate, deactivate } = defineExtension(() => {
   test.upstreamBranch.value = 'aaa'
   test.upstreamRepo.value = 'vvv'
 
-  useCommandBaseBase('project-config.manualUpdate', () => {
+  useCommandBase('project-config.manualUpdate', () => {
 
   })
 
